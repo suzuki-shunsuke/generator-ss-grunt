8275b82d170026d2b27a6e50cbdd4f1f16f45083
Suzuki Shunsuke <suzuki.shunsuke.1989@gmail.com>
2016-04-19 12:19:39 +0900

    *

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,41 +1,11 @@
 'use strict';
 var yeoman = require('yeoman-generator');
-var chalk = require('chalk');
-var yosay = require('yosay');
 
 module.exports = yeoman.Base.extend({
-
-  prompting: function () {
-    var done = this.async();
-
-    // Have Yeoman greet the user.
-    this.log(yosay(
-      'Welcome to the primo ' + chalk.red('generator-ss-grunt') + ' generator!'
-    ));
-
-    var prompts = [{
-      type: 'confirm',
-      name: 'someAnswer',
-      message: 'Would you like to enable this option?',
-      default: true
-    }];
-
-    this.prompt(prompts, function (props) {
-      this.props = props;
-      // To access props later use this.props.someAnswer;
-
-      done();
-    }.bind(this));
-  },
-
   writing: function () {
-    this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
+    this.directory(
+      this.templatePath('.'),
+      this.destinationPath('.')
     );
-  },
-
-  install: function () {
-    this.installDependencies();
   }
 });
---
+++ generators/app/templates/Gruntfile.js
@@ -0,0 +1,158 @@
+module.exports = function(grunt) {
+  'use strict';
+  grunt.initConfig({
+    bower: {
+      install: {
+        options: {
+          layout: 'byComponent',
+          cleanTargetDir: true,
+          targetDir: '',
+          install: true,
+          verbose: false,
+          cleanBowerDir: false
+        }
+      }
+    },
+    uglify: {
+      dynamic_mappings: {
+        files: [
+          {
+            expand: true,
+            cwd: '',
+            src: [''],
+            dest: '',
+            ext: '.min.js'
+          }
+        ]
+      }
+    },
+    cssmin: {
+      dynamic_mappings: {
+        files: [
+          {
+            expand: true,
+            cwd: '',
+            src: [''],
+            dest: '',
+            ext: '.min.css'
+          }
+        ]
+      }
+    },
+    mocha: {
+      test: {
+        src: ['']
+      }
+    },
+    simplemocha: {
+      options: {
+        globals: ['chai'],
+        timeout: 3000,
+        ui: 'tdd',
+        reporter: 'tap'
+      },
+
+      all: { src: [''] }
+    },
+    jsonlint: {
+      sample: {
+        src: '*.json'
+      }
+    },
+    htmlmin: {
+      dist: {
+        options: {
+          collapseWhitespace: true,
+          conservativeCollapse: true,
+          removeScriptTypeAttributes: true,
+          removeStyleLinkTypeAttributes: true,
+          minifyJS: true,
+          minifyCSS: true
+        },
+        files: {
+          '': ''
+        }
+      }
+    },
+    yamllint: {
+      all: ['*.yaml', '*.yml']
+    },
+    jsbeautifier: {
+      files: [''],
+      options: {
+        html: {
+          maxPreserveNewlines: 1,
+          indentSize: 2,
+          extraLiners: []
+        }
+      }
+    },
+    sass: {
+      dynamic_mappings: {
+        files: [
+          {
+            expand: true,
+            cwd: '',
+            src: ['**/*.sass'],
+            dest: '',
+            ext: '.css'
+          }
+        ]
+      }
+    },
+    shell: {
+      gjslint: {
+        command: 'gjslint --strict --disable 0110 -r .'
+      },
+      fixjsstyle: {
+        command: 'fixjsstyle --strict --disable 0110 -r .'
+      }
+    },
+    pylint: {
+      options: {
+        // Task-specific options go here.
+      },
+      hello: {
+        // Target-specific file lists and/or options go here.
+        src: '*.py'
+      }
+    },
+    jsdoc: {
+      dist: {
+        src: [''],
+        options: {
+          destination: ''
+        }
+      }
+    },
+    curl: {
+      '': ''
+    },
+    unzip: {
+      '': ''
+    },
+    clean: {
+      zip: ['']
+    }
+  });
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-bower-task');
+  grunt.loadNpmTasks('grunt-jsonlint');
+  grunt.loadNpmTasks('grunt-yamllint');
+  grunt.loadNpmTasks('grunt-mocha');
+  grunt.loadNpmTasks('grunt-simple-mocha');
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
+  grunt.loadNpmTasks('grunt-shell');
+  grunt.loadNpmTasks('grunt-pylint');
+  grunt.loadNpmTasks('grunt-jsdoc');
+  grunt.loadNpmTasks('grunt-curl');
+  grunt.loadNpmTasks('grunt-zip');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.registerTask('test', ['mocha', 'simplemocha']);
+  grunt.registerTask('download', ['curl', 'unzip', 'clean']);
+  grunt.registerTask('minify', ['uglify', 'cssmin']);
+  grunt.registerTask('init', ['bower:install', 'download']);
+  grunt.registerTask('pre-deploy', ['bower:install', 'download', 'minify']);
+};
